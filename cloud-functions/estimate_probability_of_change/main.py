3b4fe2f5d319889a1ef4f46f663e23c3bb768d42
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2019-09-07 13:55:04 -0700

    first working service

--- cloud-functions/estimate_probability_of_change/main.py
+++ cloud-functions/estimate_probability_of_change/main.py
@@ -2,13 +2,24 @@ import os
 import base64
 
 from google.cloud import kms
+from pymongo import MongoClient
+import urllib
 
-kms_client = kms.KeyManagementServiceClient()
-db_pass = kms_client.decrypt(
+
+KMS_CLIENT = kms.KeyManagementServiceClient()
+db_pass = KMS_CLIENT.decrypt(
     os.environ["SECRET_RESOURCE_NAME"],
     base64.b64decode(os.environ["SECRET_STRING"]),
 ).plaintext
+MONGO_CLIENT = MongoClient('mongodb+srv://trader:' +
+                           urllib.parse.quote_plus(db_pass) +
+                           '@ahmed-3jokf.gcp.mongodb.net/test')
+DB = MONGO_CLIENT['prices']
 
 
 def estimate_probability_of_change(request):
-    return db_pass[:1]
+    daily_prices = DB.daily
+    results = daily_prices.find({'name': 'FB'})
+    for result in results:
+        return str(result)
+    return ""
