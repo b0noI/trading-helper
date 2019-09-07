d1ddd5eff12bc995817279630e53ce9858af985f
Gautam Kumar <Gauta@amazon.com>
2019-09-07 14:08:03 -0700

    Base version of algorithm

--- cloud-functions/estimate_probability_of_change/main.py
+++ cloud-functions/estimate_probability_of_change/main.py
@@ -4,6 +4,7 @@ import base64
 from google.cloud import kms
 from pymongo import MongoClient
 import urllib
+import numpy
 
 
 KMS_CLIENT = kms.KeyManagementServiceClient()
@@ -17,9 +18,39 @@ MONGO_CLIENT = MongoClient('mongodb+srv://trader:' +
 DB = MONGO_CLIENT['prices']
 
 
+
+def calculatePercentChage(oldval, newval):
+    return (((newval - oldval) * 100.0)/oldval)
+
+"""
+   prices: list of prices for specific stock
+   percent: percent change user expect to happen
+   ws: Window size in term of days
+"""
+def calCulateLikelyHood(prices, percent, ws):
+    if len(prices) < ws: 
+       percentChange = calculatePercentChage(prices[0], prices[len(prices.size()-1)])
+       return percentchange >= percent
+    
+    up = 0 
+    down = 0 
+    for i in range(len(prices) - ws + 1): 
+        newPercentChange = calculatePercentChage(prices[i], prices[i+ws - 1]) 
+        if newPercentChange > percent:
+           up = up + 1 
+        else:
+           down = down + 1 
+
+    return (up*100.0)/(up+down)
+
+
 def estimate_probability_of_change(request):
     daily_prices = DB.daily
     results = daily_prices.find({'name': 'FB'})
+    prices = [] 
     for result in results:
         return str(result)
-    return ""
+        prices.append(result["price"])
+
+    # 2 % change in last 3 days
+    return calCulateLikelyHood(prices, 2, 3)
