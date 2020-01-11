ec3d23ff84388b0f98c845a7b05b8c34ffe87250
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-01-11 15:36:29 -0800

    initi refactoring

---
+++ trader/model/option_contract.py
@@ -0,0 +1,24 @@
+class OptionContract(object):
+
+  def __init__(self, ticker, option_type, target_price, expiration_date):
+    """
+
+    :param ticker: name of the ticker (example: FB)
+    :type ticker: string
+    :param option_type: type can be RIGHT_TO_BUY or RIGHT_TO_SELL
+    :type option_type: str
+    :param target_price: price
+    :type option_type: float
+    :param expiration_date: date when option expires
+    :type expiration_date: datetime
+    """
+    self.ticker = ticker
+    self.option_type = option_type
+    self.target_price = target_price
+    self.target_date = expiration_date
+
+  def __str__(self):
+    return (f"OptionContract(ticker: {self.ticker}, "
+            f"option_type: {self.option_type}, "
+            f"target_price: {self.target_price}, "
+            f"target_date: {self.target_date})")
---
+++ trader/model/option_contract_trade.py
@@ -0,0 +1,13 @@
+class OptionContractTrade(object):
+
+  def __init__(self, trade_date, original_purchasing_price, option_contract):
+    """
+
+    :param trade_date:
+    :param original_purchasing_price:
+    :param option_contract:
+    """
+    self.trade_date = trade_date
+    self.original_purchasing_price = original_purchasing_price
+    self.option_contract = option_contract
+
