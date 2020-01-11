e1cea9023fc2ffc903ddee1cd5ca0f741df3aea2
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2020-01-11 15:48:27 -0800

    Update option_contract.py

--- trader/model/option_contract.py
+++ trader/model/option_contract.py
@@ -1,6 +1,6 @@
 class OptionContract(object):
 
-  def __init__(self, ticker, option_type, target_price, expiration_date):
+  def __init__(self, ticker, option_type, target_price, expiration_date, contract_price):
     """
 
     :param ticker: name of the ticker (example: FB)
@@ -11,11 +11,14 @@ class OptionContract(object):
     :type option_type: float
     :param expiration_date: date when option expires
     :type expiration_date: datetime
+    :param contract_price: price for this contract
+    :type contract_price: float
     """
     self.ticker = ticker
     self.option_type = option_type
     self.target_price = target_price
     self.target_date = expiration_date
+    self.contract_price = contract_price
 
   def __str__(self):
     return (f"OptionContract(ticker: {self.ticker}, "
